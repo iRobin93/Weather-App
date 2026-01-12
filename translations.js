39e84a5d5cb32e0f9557c5577d49635b62afabf8
Robin <61976533+iRobin93@users.noreply.github.com>
2026-01-12 13:15:51 +0100

    updates

--- translations.js
+++ translations.js
@@ -1,45 +1,50 @@
 export class Translations {
-            constructor(language = "en-EN") {
-                this.language = language;
+    constructor(language = "en-EN") {
+        this.language = language;
 
-                const allTexts = {
-                    "en-EN": {
-                        settings: "Settings",
-                        location: "Location",
-                        appBackgroundColor: "App Background color",
-                        cardBackgroundColor: "Weather-Card Background color",
-                        textColor: "Text color",
-                        language: "Language",
-                        days: "Days to show",
-                        celsius: "Show Celsius",
-                        save: "Save",
-                        cancel: "Cancel",
-                        wind: "Wind",
-                        english: "English",
-                        norwegian: "Norwegian",
-                        PlaceNotFoundError: "Could not find location",
+        const allTexts = {
+            "en-EN": {
+                settings: "Settings",
+                location: "Location",
+                appBackgroundColor: "App Background color",
+                cardBackgroundColor: "Weather-Card Background color",
+                textColor: "Text color",
+                language: "Language",
+                days: "Days to show",
+                celsius: "Show Celsius",
+                save: "Save",
+                cancel: "Cancel",
+                english: "English",
+                norwegian: "Norwegian",
+                placeNotFoundError: "Could not find location",
+                precipitation: "Precipitation",
+                humidity: "Humidity",
+                wind: "Wind",
 
-                    },
-                    "nb-NO": {
-                        settings: "Innstillinger",
-                        location: "Sted",
-                        appBackgroundColor: "App Bakgrunnsfarge",
-                        cardBackgroundColor: "Vær-Kort Bakgrunnsfarge",
-                        textColor: "Tekstfarge",
-                        language: "Språk",
-                        days: "Vis dager",
-                        celsius: "Vis Celsius",
-                        save: "Lagre",
-                        cancel: "Avbryt",
-                        wind: "Vind",
-                        english: "Engelsk",
-                        norwegian: "Norsk",
-                        PlaceNotFoundError: "Fant ikke stedet",
-                    },
-                    // Add more languages here
-                };
 
-                // Default to Browser language - or English if browser language is not valid
-                this.texts = allTexts[language] || allTexts[navigator.language] || allTexts["en-EN"];
-            }
-        }
+            },
+            "nb-NO": {
+                settings: "Innstillinger",
+                location: "Sted",
+                appBackgroundColor: "App Bakgrunnsfarge",
+                cardBackgroundColor: "Vær-Kort Bakgrunnsfarge",
+                textColor: "Tekstfarge",
+                language: "Språk",
+                days: "Vis dager",
+                celsius: "Vis Celsius",
+                save: "Lagre",
+                cancel: "Avbryt",
+                english: "Engelsk",
+                norwegian: "Norsk",
+                PlaceNotFoundError: "Fant ikke stedet",
+                precipitation: "Nedbør",
+                humidity: "Luftfuktighet",
+                wind: "Vind",
+            },
+            // Add more languages here
+        };
+
+        // Default to Browser language - or English if browser language is not valid
+        this.texts = allTexts[language] || allTexts[navigator.language] || allTexts["en-EN"];
+    }
+}
